5a4dbd3dbbebdb496699456ec0afbbfe132dd005
Manav <75903971+notendurable@users.noreply.github.com>
2021-11-28 21:51:32 +0300

    Create Conclusion.jsx

---
+++ client/src/Conclusion.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import NavigationBar from './NavigationBar';
+
+const ReactInstallTutorial = () => {
+  return (
+    <div>
+      <NavigationBar />
+      <br />
+	    <div className='row'>
+		    <div className='col'>
+			    <h1 className='text-center'>How to install ReactJS on Windows</h1>
+		    </div>
+	    </div>
+	    <div className= 'row'>
+	  	    <div className='col-3'></div>
+	  	    <div className='col-6'></div>
+	  		<hr />
+	  		
+        </div>
+	</div>
+  );
+}
